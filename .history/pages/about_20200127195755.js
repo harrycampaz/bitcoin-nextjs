ddf79e31fb1a4e20cfc608d4f630a464c733f184
harrycampaz <harrycampaz@gmail.com>
2020-01-27 20:00:14 -0500

    Nextjs Impro

---
+++ .history/pages/about_20200127195755.js
@@ -0,0 +1,7 @@
+
+
+const about = () => (
+            <h1>Total</h1>
+    );
+
+export default about;
--- .next/static/runtime/webpack.js
+++ .next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "9df021bb86e1145ee618";
+/******/ 	var hotCurrentHash = "4be6b9f591b6d45d15c2";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
